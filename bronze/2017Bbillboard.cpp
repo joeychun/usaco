58eae631cddd72489f7f9a4ecfd5e6ff4685a7b1
Joey Chun <joey.s.chun@gmail.com>
2019-09-29 19:25:01 +0900

    Added algorithm for billboard.cpp

--- bronze/2017Bbillboard.cpp
+++ bronze/2017Bbillboard.cpp
@@ -1,3 +1,39 @@
+/*
+ALGORITHM :
+Area that can be seen = (total area of billboards) - (covered area)
+                      = (area of billboard1) + (area of billboard2) - (covered area from billboard1) - (covered area from billboard2)
+
+Billboard Area(x1,y1, x2,y2) = (x2-x1) * (y2-y1)  << Basic length * width area formula
+
+Covered Area:
+when Billboard is made out of (x1,y1), (x2,y2),
+ CoveringTruck is made out of (X1,Y1), (X2.Y2).
+
+ex)   ___________________________
+      |                          |
+      |Truck                     |
+______|_____________             |
+|     |____________|_____________|
+|                  |
+|Billboard         |
+|__________________|
+x1   X1            x2            X2
+
+Length of Covered Area (when rectangles are overlapped)
+= x2-X1
+In other words, min(x2,X2) - max(x1,X1).
+This works both when the lengths of the rectangles are partly overlapping and completely overlapping.
+
+Length of Covered Area (when rectangles are overlapped)
+= 0
+
+Therefore, Length of Covered Area = max(min(x2,X2) - max(x1,X1), 0)
+Width can be done in the same way with y.
+
+Then, covered Area can be calculated with length * width as well.
+
+*/
+
 #include <iostream>
 #include <fstream>
 
@@ -21,8 +57,10 @@ int main(){
     ifs >> Rect[i].x1 >> Rect[i].y1 >> Rect[i].x2 >> Rect[i].y2;
   }
 
+  // TOTAL AREA
   int total_area = (Rect[0].x2-Rect[0].x1)*(Rect[0].y2-Rect[0].y1) + (Rect[1].x2-Rect[1].x1)*(Rect[1].y2-Rect[1].y1);
 
+  // COVERED AREA
   int xlen, ylen;
   for (i=0; i<=1; ++i) {
     // Covered area in billboardi
@@ -34,6 +72,7 @@ int main(){
     max = min(Rect[i].y2,Rect[2].y2);
     ylen = max(0,(max-min));
 
+    // DEDUCTING COVERED AREA
     total_area -= xlen*ylen;
   }
 
